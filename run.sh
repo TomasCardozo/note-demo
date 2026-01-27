6783cb483b040ce55a42aada9d1659112a9964d1
TomasCardozo <tomytala23@gmail.com>
2026-01-27 10:51:37 -0300

    run.sh fixed

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@
 set -e
 
 docker compose up -d db
+DB_CONTAINER=$(docker compose ps -q db)
+
+until docker exec "$DB_CONTAINER" mysqladmin ping -uroot -proot --silent; do
+  sleep 2
+done
+
 
 cd backend
 ./mvnw spring-boot:run
